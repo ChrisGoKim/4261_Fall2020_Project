e61662a4720cabc9a3b5942dae4be141561719c7
Tony Z Tan <tony@tonytan.io>
2020-10-21 19:48:04 -0400

    back up aws lamda code to repo

---
+++ aws_lambda_src/test_hello.js
@@ -0,0 +1,21 @@
+console.log('Loading function');
+
+exports.handler = async (event, context, callback) => {
+    let min = 0;
+    let max = 10
+
+    let genNum = Math.floor(Math.random() * max) + min;
+
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(genNum),
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "http://localhost:8080",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
+
+        }});
+
+    return genNum;
+};
