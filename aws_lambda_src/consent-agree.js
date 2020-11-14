f2197f5a37751ac261c4766bad0568763e3c537c
Tony Z Tan <tony@tonytan.io>
2020-11-13 20:12:21 -0500

    upload DJ's lambda code

---
+++ aws_lambda_src/consent-agree.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+/**
+ * Gets user's consent from the User table
+ *
+ */
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,PUT,GET"
+    };
+
+    try {
+        switch (event.httpMethod) {
+            case 'PUT':
+                const message = JSON.parse(event.body);
+                var user = message.user
+
+                const params = {
+                    TableName: "User",
+                    Item: {
+                        Username: user,
+                        consent: true
+                    }
+                };
+
+                body = await dynamo.put(params).promise();
+
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
